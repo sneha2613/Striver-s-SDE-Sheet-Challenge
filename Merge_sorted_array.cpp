d280b4bde84807ed9e58698023208c93219b7408
Sneha Das <snehads2613@gmail.com>
2022-06-09 19:34:58 +0530

    Brute force here

---
+++ Merge_sorted_array.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+vector<int> ninjaAndSortedArrays(vector<int>& arr1, vector<int>& arr2, int m, int n) {
+	// Write your code here.
+    vector<int> ans;
+    for(int i=0;i<m ;i++){
+        if(arr1[i]!=0)
+            ans.push_back(arr1[i]);
+    }
+    for(int i=0;i<n ;i++){
+        if(arr2[i]!=0)
+            ans.push_back(arr2[i]);
+    }
+    sort(ans.begin(), ans.end());
+    return ans;
+}
